a25636720fe0ed5dfcc29990c035e3621a2b7254
vivamus <vivamus@github.com>
2022-07-22 14:17:15 -0700

    configparser

--- ch02-prepare-task.py
+++ ch02-prepare-task.py
@@ -1,4 +1,5 @@
 import argparse
+import configparser
 
 def main(n1,n2):
     result = n1 * n2
@@ -8,5 +9,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n1', type=int, help='num 1', default=1)    
     parser.add_argument('-n2', type=int, help='num 2', default=1)
+    parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='config file')
     args = parser.parse_args()
+    if args.config:
+        config = configparser.ConfigParser()
+        config.read_file(args.config)
+        args.n1 = int(config['ARGUMENTS']['n1'])
+        args.n2 = int(config['ARGUMENTS']['n2'])
     main(args.n1, args.n2)
